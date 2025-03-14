c1dfd8929055bb73a638cadd2ff6491c0b330c35
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-13 21:53:55 -0400

    Delete shuffleandprogram.js

--- shuffleandprogram.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('shuffle').addEventListener('click', function() {
-    console.log('Shuffle button clicked');
-});
-
-document.getElementById('program').addEventListener('click', function() {
-    console.log('Program button clicked');
-});
