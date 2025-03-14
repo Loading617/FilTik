5d1990d4377c136f23e3c3572586efb17223a9f1
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-13 20:38:12 -0400

    Add files via upload

---
+++ shuffleandprogram.js
@@ -0,0 +1,7 @@
+document.getElementById('shuffle').addEventListener('click', function() {
+    console.log('Shuffle button clicked');
+});
+
+document.getElementById('program').addEventListener('click', function() {
+    console.log('Program button clicked');
+});
