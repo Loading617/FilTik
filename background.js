3ee6af3dcad3daefbe5d3552ce8dc3ffbfb29c89
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-14 12:53:21 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,18 +1,3 @@
-function fetchData(url, callback) {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => callback(data))
-        .catch(error => console.error('Error fetching data:', error));
-}
-
-function initBackgroundTasks() {
-    console.log('Background tasks initialized');
-
-    setInterval(() => {
-        fetchData('https://api.example.com/data', (data) => {
-            console.log('Fetched data:', data);
-        });
-    }, 60000);
-}
-
-initBackgroundTasks();
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("TikTok Video Organizer installed.");
+});
