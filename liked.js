12da033391e7da17deb9046a7146435d81fdcce7
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-14 12:40:35 -0400

    Delete liked.js

--- liked.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('profile-buttons').innerHTML = `
-    <div class="filter-buttons">
-        <button id="oldest">Oldest</button>
-        <button id="popular">Popular</button>
-        <button id="newest">Newest</button>
-        <button id="shuffle">Shuffle</button>
-        <button id="program">Program</button>
-    </div>
-`;
